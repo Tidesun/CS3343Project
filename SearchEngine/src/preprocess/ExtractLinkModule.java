1ca18bf92f422efb2a9cb871e47f87b7fdf09777
liu1322592019 <40196261+liu1322592019@users.noreply.github.com>
2018-11-03 20:37:09 +0800

    Add files via upload

--- SearchEngine/src/preprocess/ExtractLinkModule.java
+++ SearchEngine/src/preprocess/ExtractLinkModule.java
@@ -8,15 +8,19 @@ import java.util.regex.Matcher;
 public class ExtractLinkModule extends ExtractModuleAbstract implements ExtractModuleInterface{
 	public String WebPageExtraction(File filename) throws FileNotFoundException {
 		String content=this.getContent(filename);
-		Pattern pattern = Pattern.compile("<a\\s*href=\"?([\\w\\W]*?)\"", Pattern.CASE_INSENSITIVE);
+		Pattern pattern = Pattern.compile("<a.*href=\"?([\\w\\W]*?)\"", Pattern.CASE_INSENSITIVE);
 		Matcher matcher = pattern.matcher(content);
 		String link_str = "";
 		
 		while (matcher.find()) {
 			String the_str = matcher.group(1);			
 			String last_part = the_str.substring(the_str.lastIndexOf("/")+1);		
-			if(the_str.contains("cityu.edu.hk") && the_str.contains("http") && last_part.matches("[^#@<>']*$") && (last_part.contains(".htm") || !last_part.contains("."))) {
-				link_str += the_str.substring(the_str.indexOf("://")+3) + " ";
+			if(the_str.contains("cityu.edu.hk") && the_str.contains("http") && last_part.matches("[^@<>']*$") && (last_part.contains(".htm") || !last_part.contains("."))) {
+				if(last_part.contains("#")) {
+					link_str += the_str.substring(the_str.indexOf("://")+3, the_str.lastIndexOf("#"))+ " ";
+				}else {
+					link_str += the_str.substring(the_str.indexOf("://")+3) + " ";
+				}
 			}
 		}
 		return link_str;
