872c9e1254282c99a9c4dba1df5db300a745dec2
liu1322592019 <40196261+liu1322592019@users.noreply.github.com>
2018-11-17 15:22:06 +0800

    Add files via upload

--- SearchEngine/src/testPreprocess/TestInvertedIndexModule.java
+++ SearchEngine/src/testPreprocess/TestInvertedIndexModule.java
@@ -3,24 +3,36 @@ package testPreprocess;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.Before;
+import org.junit.Rule;
 
 import preprocess.*;
 import java.util.HashMap;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TestInvertedIndexModule {
+	
+	@Rule
+	public TemporaryFolder temp= new TemporaryFolder();
+	File file1;
+	File file2;
 	HashMap<String, ArrayList<String>> forward;
 	InvertedIndexModuleInterface inverted;
 	
 	@Before
 	public void setUp() {
+		file1 = new File(temp.getRoot(), "InvertedIndexDataset");
+		file2 = new File(temp.getRoot(), "ForwardIndexDataset");
+		String path1 = (String) file1.getPath();
+		String path2 = (String) file2.getPath();
 		forward = new HashMap<>();
 		StringMapModule urlExtractor=new StringMapModule(new ExtractLinkModule());
-		ForwardIndexModuleInterface forwardLinkMod=new ForwardIndexModule(urlExtractor,"src/res/dataset/linkForwardIndexDataset");
-		inverted=new InvertedIndexModule(forwardLinkMod,"src/res/dataset/linkInvertedIndexDataset");
+		ForwardIndexModuleInterface forwardLinkMod=new ForwardIndexModule(urlExtractor,path1);
+		inverted=new InvertedIndexModule(forwardLinkMod,path2);
 	}
 	
 	// new empty hashmap
@@ -35,15 +47,12 @@ public class TestInvertedIndexModule {
 	public void testGenerateInvertedIndexMap2() throws IOException {
 		// parameter
 		forward.put("github.html", new ArrayList<>(Arrays.asList("github")));
-		forward.put("github pages", new ArrayList<>(Arrays.asList("github")));
-		forward.put("airbnb.html", new ArrayList<>(Arrays.asList("airbnb")));
 		
 		inverted.generateInvertedIndexMap(forward);
 
-		// result
+		// result 
 		HashMap<String, ArrayList<String>> res = new HashMap<>();
-		res.put("github", new ArrayList<>(Arrays.asList("github.html", "github pages")));
-		res.put("airbnb", new ArrayList<>(Arrays.asList("airbnb.html")));
+		res.put("github", new ArrayList<>(Arrays.asList("github.html")));
 		
 		assertEquals(res, inverted.getInvertedIndexMap());
 	}
@@ -52,18 +61,41 @@ public class TestInvertedIndexModule {
 	@Test
 	public void testGenerateInvertedIndexMap3() throws IOException {
 		// parameter
-		forward.put("github.html", new ArrayList<>(Arrays.asList("github", "git")));
-		forward.put("airbnb.html", new ArrayList<>(Arrays.asList("airbnb", "air")));
+		forward.put("github.html", new ArrayList<>(Arrays.asList("github")));
+		forward.put("github pages", new ArrayList<>(Arrays.asList("github")));
 		
 		inverted.generateInvertedIndexMap(forward);
 
 		// result
 		HashMap<String, ArrayList<String>> res = new HashMap<>();
-		res.put("github", new ArrayList<>(Arrays.asList("github.html")));
-		res.put("git", new ArrayList<>(Arrays.asList("github.html")));
-		res.put("air", new ArrayList<>(Arrays.asList("airbnb.html")));
-		res.put("airbnb", new ArrayList<>(Arrays.asList("airbnb.html")));
+		
+		ArrayList<String> urlsList = new ArrayList<String>();
+		urlsList.add("github.html");
+		urlsList.add("github pages");
+		res.put("github", urlsList);
 		
 		assertEquals(res, inverted.getInvertedIndexMap());
 	}
+	 
+	
+	
+	// normal hashmap with content
+	@Test
+	public void testGenerateInvertedIndexMap4() throws IOException {
+		// parameter
+		forward.put("github.html", new ArrayList<>(Arrays.asList("github")));
+		forward.put("github.html", new ArrayList<>(Arrays.asList("github")));
+		
+		inverted.generateInvertedIndexMap(forward);
+
+		// result
+		HashMap<String, ArrayList<String>> res = new HashMap<>();
+		
+		ArrayList<String> urlsList = new ArrayList<String>();
+		urlsList.add("github.html");;
+		res.put("github", urlsList);
+		
+		assertEquals(res, inverted.getInvertedIndexMap());
+	}
+	 
 }
